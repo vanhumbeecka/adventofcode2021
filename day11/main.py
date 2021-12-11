726a7401d7f254b42ff205b8d59ccf873bb9b2ed
Andries Van Humbeeck <andries.van.humbeeck@scaledaccess.com>
2021-12-11 14:52:04 +0100

    day11

---
+++ day11/main.py
@@ -0,0 +1,74 @@
+import dataclasses
+from typing import List, Tuple
+from pathlib import Path
+from octopus import Octopus, Coord
+
+import numpy as np
+from collections import deque
+
+
+class InvalidStateError(Exception):
+    pass
+
+
+def read_file(name) -> List[Octopus]:
+    base_path = Path(__file__).parent
+    file_path = (base_path / name).resolve()
+    lines: List[str] = []
+    with open(file_path) as f:
+        for line in f:
+            line = line.rstrip('\n')
+            lines.append(line)
+
+    max_row = len(lines)
+    max_col = len(lines[0])
+
+    octopus_list: List[Octopus] = []
+    for row in range(max_row):
+        line = list(lines[row])
+        for i in range(len(line)):
+            octopus = Octopus(int(line[i]), Coord(row, i))
+            octopus_list.append(octopus)
+
+    for octopus in octopus_list:
+        neighbour_coords = octopus.coord.get_neighbours(max_row, max_col)
+        neighbours = [n for n in octopus_list if n.coord in neighbour_coords]
+        octopus.set_neighbours(neighbours)
+
+    return octopus_list
+
+def algo_1(octopus_list: List[Octopus]):
+    for i in range(100):
+        for o in octopus_list:
+            o.increase_energy()
+        for o in octopus_list:
+            o.reset_step()
+
+    flash_count = 0
+    for o in octopus_list:
+        flash_count += o.flash_count
+
+    return flash_count
+
+def algo_2(octopus_list: List[Octopus]):
+    step = 0
+    while True:
+        step += 1
+        for o in octopus_list:
+            o.increase_energy()
+
+        flashed = [o.flashed for o in octopus_list]
+        if all(flashed):
+            return step
+
+        for o in octopus_list:
+            o.reset_step()
+
+
+
+if __name__ == '__main__':
+    file = 'input.txt'
+    my_list = read_file(file)
+    print(algo_1(my_list))
+    my_list_2 = read_file(file)
+    print(algo_2(my_list_2))
---
+++ day11/octopus.py
@@ -0,0 +1,63 @@
+import dataclasses
+from typing import List
+
+
+@dataclasses.dataclass
+class Coord:
+    row: int
+    col: int
+
+    def __eq__(self, other):
+        return self.row == other.row and self.col == other.col
+
+    def get_neighbours(self, max_row: int, max_col: int):
+        neighbours = [
+            Coord(self.row - 1, self.col) if self.row > 0 else None,
+            Coord(self.row - 1, self.col + 1) if self.row > 0 and self.col < max_col else None,
+            Coord(self.row, self.col + 1) if self.col < max_col else None,
+            Coord(self.row + 1, self.col + 1) if self.row < max_row and self.col < max_col else None,
+            Coord(self.row + 1, self.col) if self.row < max_row else None,
+            Coord(self.row + 1, self.col - 1) if self.row < max_row and self.col > 0 else None,
+            Coord(self.row, self.col - 1) if self.col > 0 else None,
+            Coord(self.row - 1, self.col - 1) if self.col > 0 and self.row > 0 else None
+        ]
+
+        return [i for i in neighbours if i is not None]
+
+
+
+class Octopus:
+    def __init__(self, energy: int, coord: Coord):
+        self.coord = coord
+        self.neighbours = []
+        self.energy = energy
+        self.flashed = False
+        self.flash_count = 0
+
+    def __eq__(self, other):
+        return self.coord == other.col
+
+    def __repr__(self):
+        return f"<Octopus energy={self.energy} {self.coord} neighbours={len(self.neighbours)} flash_count={self.flash_count}>"
+
+    def set_neighbours(self, neighbours: List["Octopus"]):
+        self.neighbours = neighbours
+
+    def reset_step(self):
+        self.flashed = False
+
+    def _flash(self):
+        self.energy = 0
+        self.flashed = True
+        self.flash_count += 1
+        for neighbour in self.neighbours:
+            neighbour.increase_energy()
+
+    def increase_energy(self):
+        if self.flashed:
+            return
+
+        if self.energy < 9:
+            self.energy += 1
+        else:
+            self._flash()
